23ecb645623526f71e0a71ff7343daf0f708d31a
Sergey Razuev <sergei_razuev@epam.com>
2022-09-05 20:49:58 +0300

    Memento pattern realized

--- Trainings/src/GoF_Pattrens/Behavioral/Command/Client.java
+++ Trainings/src/GoF_Pattrens/Behavioral/Command/Client.java
@@ -3,10 +3,7 @@ package GoF_Pattrens.Behavioral.Command;
 public class Client {           //client
     public static void main(String[] args) {
         Light light = new Light();
-        TurnOnLightCommand turnOnLightCommand = new TurnOnLightCommand(light);
-        TurnOffLightCommand turnOffLightCommand = new TurnOffLightCommand(light);
-
-        Switcher switcher = new Switcher(turnOnLightCommand, turnOffLightCommand);
+        Switcher switcher = new Switcher(new TurnOnLightCommand(light), new TurnOffLightCommand(light));
 
         System.out.println("в комнате темно. Включим свет.");
         switcher.turnON();
