647e2f6ef7fe8368f853f7af44c5fd83bab29597
Sergey Razuev <sergei_razuev@epam.com>
2022-09-17 18:41:20 +0300

    Simple Builder imnplemented

--- Trainings/src/GoF_Pattrens/Behavioral/Memento/Caretaker.java
+++ Trainings/src/GoF_Pattrens/Behavioral/Memento/Caretaker.java
@@ -2,10 +2,10 @@ package GoF_Pattrens.Behavioral.Memento;
 
 public class Caretaker {                //опекун. Хранит переданные ему снимки
     Memento memento;
-    public Memento getMemento(){        //восстанавливаем сохранение снимка, т.е. загружаем сохранение игры
+    public Memento getMemento(){        //получаем снимок, т.е. сохраняем игру
         return memento;
     }
-    public void setMemento(Memento memento){        //получаем снимок, т.е. сохраняем игру
+    public void setMemento(Memento memento){       //восстанавливаем сохранение снимка, т.е. загружаем сохранение игры
         this.memento=memento;
     }
 }
