334d5f8cd44d4b828861e8614e2c93e9234113df
Sergey Razuev <sergei_razuev@epam.com>
2023-04-19 16:58:57 +0300

    new switch case

--- src/switch_test/SwitchTest.java
+++ src/switch_test/SwitchTest.java
@@ -7,22 +7,13 @@ public class SwitchTest {
     }
 
     public int defineLegs(Pet pet) {
-        int legs;
-
-        switch (pet) {
-            case DOG:
-            case CAT:
-                legs = 4;
-                break;
-            case PARROT:
-                legs = 2;
-                break;
-            case GOLDFISH:
-                legs = 0;
-                break;
-            default:
-                throw new AssertionError("Неопределенное количество ног");
-        }
+        int legs = switch (pet) { //теперь весь свитч это просто объявление переменной legs
+            case DOG, CAT -> legs = 4;
+            case PARROT -> legs = 2;
+            case GOLDFISH -> legs = 0;
+//            default: //не нужен, поскольку покрыты все варианты енама
+//                throw new AssertionError("Неопределенное количество ног");
+        };
         return legs;
     }
 }
